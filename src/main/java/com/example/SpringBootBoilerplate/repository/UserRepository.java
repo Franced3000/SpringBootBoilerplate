221a6bb3d117d5931b8d981202b6908c32df6eb3
Franced3000 <franc.spata@gmail.com>
2024-07-25 09:43:53 +0200

    user repo

---
+++ src/main/java/com/example/SpringBootBoilerplate/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootBoilerplate.repository;
+
+import com.example.SpringBootBoilerplate.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
